e2ebd82af49344b95ff7013179a6419a8025f4ec
lucia6758 <lucia6758@gmail.com>
2020-12-23 12:27:40 +0900

    배포

--- article/app.js
+++ article/app.js
@@ -10,8 +10,30 @@ function ArticleDetail_body_init() {
 		el : body,
 		initialValue : initialValue,
 		viewer : true,
-		plugins: [toastui.Editor.plugin.codeSyntaxHighlight] 
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight, codepenPlugin] 
 	});
 }
 
 ArticleDetail_body_init();
+
+// codepen 플러그인 시작
+function codepenPlugin() {
+  toastui.Editor.codeBlockManager.setReplacer('codepen', url => {
+    const wrapperId = `yt${Math.random().toString(36).substr(2, 10)}`;
+
+    // Avoid sanitizing iframe tag
+    setTimeout(renderCodepen.bind(null, wrapperId, url), 0);
+
+    return `<div id="${wrapperId}"></div>`;
+  });
+}
+
+function renderCodepen(wrapperId, url) {
+  const el = document.querySelector(`#${wrapperId}`);
+  
+  var urlParams = new URLSearchParams(url.split('?')[1]);
+  var height = urlParams.get('height');
+
+  el.innerHTML = `<div class="toast-ui-codepen-plugin-wrap"><iframe height="${height}" scrolling="no" src="${url}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe></div>`;
+}
+// codepen 플러그인 끝
